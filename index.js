f515f4d4ea08fb4417168f79b3074e3c3cc75717
Adam Yiu <adam_yiu@outlook.com>
2022-08-14 21:52:21 -0400

    Add rectangle downward movement

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ c.fillRect(0, 0,canvas.width, canvas.height);
 class Sprite {
   // Every class needs a constructor, is triggered everytime an instance of class is called
   // Creating property within a class constructor, must be prefaced with 'this'
-  constructor(position) {
+  // Passing it in as an object will not require a specific parameter order when creating an instance
+  // Velocity has x and y (2 dimensional, left/right & up/down )
+  constructor({position, velocity}) {
     this.position = position;
+    this.velocity = velocity
   }
 
   // Arbitrarily named, can be named whatever
@@ -27,19 +30,49 @@ class Sprite {
     c.fillStyle = 'honeydew'
     c.fillRect(this.position.x, this.position.y, 50, 150);
   }
+
+  // Updates sprites position
+  update() {
+    this.draw();
+    this.position.y += 10
+  }
 }
 // New instance of Spirte class, starting position is 0, 0
+// Velocity is default at 0, 0. Means character will not move by default
 const player = new Sprite({
-  x: 0,
-  y: 0
+  position: {
+    x: 0,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
 });
 
-const enemy = new Sprite({
-  x: 900,
-  y: 0
-})
-// Calling the draw method defined in Sprite
-player.draw();
-enemy.draw();
+const enemy  = new Sprite({
+  position: {
+    x: 500,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
+});
+
+// Adding gravity
+// Creating an infinite loop to animate frame by frame
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = 'salmon'
+  // Prevents 'paint' effect
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  // Draw is already called in update
+  player.update();
+  enemy.update();
+}
+
+animate();
 
 console.log(player)
