9b2b99adec770748330d260288084827025bce37
Adam Yiu <adam_yiu@outlook.com>
2022-08-16 22:29:24 -0400

    add attack box

--- index.js
+++ index.js
@@ -21,10 +21,17 @@ class Sprite {
   // Creating property within a class constructor, must be prefaced with 'this'
   // Passing it in as an object will not require a specific parameter order when creating an instance
   // Velocity has x and y (2 dimensional, left/right & up/down )
-  constructor({position, velocity}) {
+  constructor({position, velocity, color}) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.color = color;
+    this.attackBox = {
+      // Attack box follows the sprite's location
+      position: this.position,
+      width: 100,
+      height: 50
+    };
     // Allows for more accurate movement tracking (i.e. if d is held down, then a is held while d is still held down)
     this.lastKeyPressed = "";
   }
@@ -32,8 +39,12 @@ class Sprite {
   // Arbitrarily named, can be named whatever
   draw() {
     // Color must be defined BEFORE marking its position
-    c.fillStyle = 'honeydew'
+    c.fillStyle = this.color
     c.fillRect(this.position.x, this.position.y, 50, this.height );
+
+    // Draw attack box
+    c.fillStyle = 'purple'
+    c.fillRect(this.position.x, this.position.y, this.attackBox.width, this.attackBox.height)
   }
 
   // Updates sprites position
@@ -66,7 +77,8 @@ const player = new Sprite({
   velocity: {
     x: 0,
     y: 10
-  }
+  },
+  color: 'green'
 });
 
 const enemy  = new Sprite({
@@ -77,7 +89,8 @@ const enemy  = new Sprite({
   velocity: {
     x: 0,
     y: 10
-  }
+  },
+  color: 'yellow'
 });
 
 const keys = {
@@ -101,9 +114,6 @@ const keys = {
   },
 };
 
-
-
-
 // Adding gravity
 // Creating an infinite loop to animate frame by frame
 function animate() {
@@ -115,7 +125,7 @@ function animate() {
   // Draw is already called in update
   player.update();
   enemy.update();
-
+  // Stops sprites from moving if key is released
   player.velocity.x = 0;
   enemy.velocity.x = 0;
 
